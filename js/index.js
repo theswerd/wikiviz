b340af705166455d3aea7363d3a6c1b940957c37
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2021-02-28 22:32:40 -0800

    continued

--- js/index.js
+++ js/index.js
@@ -10,12 +10,11 @@ document.getElementById("baseURL").addEventListener("input", (event) => {
   event.target.size =
     event.target.value.length < 10 ? 10 : event.target.value.length;
 
-    if(event.target.value.length >= 1) {
-        document.getElementById('getConnections').className = "enabled";
-    }else{
-        document.getElementById('getConnections').className = "disabled";
-
-    }
+  if (event.target.value.length >= 1) {
+    document.getElementById("getConnections").className = "enabled";
+  } else {
+    document.getElementById("getConnections").className = "disabled";
+  }
 });
 
 document.getElementById("depth").addEventListener("input", (event) => {
@@ -27,5 +26,29 @@ document.getElementById("depth").addEventListener("input", (event) => {
 });
 
 let getConnections = () => {
-  console.log("Connected");
+  if (document.getElementById("getConnections").className == "enabled") {
+    const url = document.getElementById("baseURL").value;
+
+    console.log(url);
+
+    const loading = document.getElementById("loadingMessage");
+    loading.innerText = "Loading " + url;
+    fetch(
+      "https://en.wikipedia.org/w/api.php?action=query&generator=links&gpllimit=500&origin=*&format=json&formatversion=2&titles=" +
+        url,
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((json) => {
+                  loading.innerText = "Loaded: " + json;
+
+        console.log(json);
+      })
+      .catch((error) => {
+        loading.innerText = "Unable to load: " + error.message;
+        console.log(error.message);
+      });
+  }
 };
